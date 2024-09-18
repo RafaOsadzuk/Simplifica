1cfef45a3bcdd134de2a48c52993228d4f1a0422
Rafael Osadzuk <rafael.osadzuk@gmail.com>
2024-09-17 23:09:25 -0300

    criando o back end em spring framework

---
+++ src/model/Simplifica/src/test/java/com/example/Simplifica/SimplificaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Simplifica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplificaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
