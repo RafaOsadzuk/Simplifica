ca3aa3497dfeec3dadfd0e062f5152442c010948
Rafael Osadzuk <rafael.osadzuk@gmail.com>
2024-09-19 17:49:39 -0300

    recriando o projeto spring em gradle

--- src/model/demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/model/demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.Simplifica;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimplificaApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
