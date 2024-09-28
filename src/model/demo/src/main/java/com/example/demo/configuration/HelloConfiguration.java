b10e9766d681e5d26bc4f8f314f4a852e62cd6d7
Rafael Osadzuk <rafael.osadzuk@gmail.com>
2024-09-27 23:05:35 -0300

    criando um hello world no Spring

---
+++ src/model/demo/src/main/java/com/example/demo/configuration/HelloConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloConfiguration {
+
+}
---
+++ src/model/demo/src/main/java/com/example/demo/controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/hello-world")
+public class HelloWorldController {
+
+    @Autowired
+    private HelloWorldService helloWorldService;
+
+    @GetMapping
+    public String helloWorld() {
+    return helloWorldService.helloWorld("Rafael");
+    }
+}
